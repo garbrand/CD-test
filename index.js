9797e04a06ea9d9d69edf188830d3609e7266bcf
garbrand <garbrand@gmail.com>
2013-02-13 21:36:41 +0100

    Integrated seaport -- where is node_modules?

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var http = require('http');
-var port = 8000;
+var http    = require('http');
+var seaport = require('seaport');
+
+var ports   = seaport.connect('localhost', 7070);
+var port    = ports.register('app@0.0.1');
 
 var server = http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.end("Hello World from port " + port);
 });
 
-server.listen(port);
+server.listen( port );
 
 console.log('Server running on port %s', port);
