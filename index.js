c121ebba8859135ef0c5e39377f1a6899305fcd2
garbrand <garbrand@gmail.com>
2013-02-13 21:25:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var port = 8000;
+
+var server = http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.end("Hello World from port " + port);
+});
+
+server.listen(port);
+
+console.log('Server running on port %s', port);
