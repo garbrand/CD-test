1092e2d981d9863838927399e95b1726103ae6a9
garbrand <garbrand@gmail.com>
2013-02-14 15:56:29 +0100

    Added a process title

--- substack/index.js
+++ substack/index.js
@@ -1,10 +1,18 @@
-var http    = require('http');
-var seaport = require('seaport');
-var version = require('./package.json').version;
+var http      = require('http');
+var seaport   = require('seaport');
+var pkg       = require('./package.json');
 
-var ports   = seaport.connect('localhost', 7070);
-var port    = ports.register('app@' + version);
+// Naming
+var version   = pkg.version;
+var service   = pkg.name;
 
+process.title = service+'@'+version;
+
+// Ports
+var ports     = seaport.connect('localhost', 7070);
+var port      = ports.register('app@' + version);
+
+// Spin up the service
 var server = http.createServer( function(request, response) {
 	response.end("App version " + version + " from port " + port);
 } );
